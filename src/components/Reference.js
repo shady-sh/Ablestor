e02f8bd432f84838802a9a401176dbe45b515f08
Marshall Oh <dhtmdgusxx@naver.com>
2021-01-19 15:28:31 +0900

    Reference Complete

--- src/components/Reference.js
+++ src/components/Reference.js
@@ -13,7 +13,7 @@ const category = [
   { id: 5, name: "종교/사회단체" },
 ];
 
-let edu_item = [];
+let items = [];
 
 export default class Reference extends Component {
   constructor(props) {
@@ -48,60 +48,255 @@ export default class Reference extends Component {
         </a>
       </li>
     ));
-  addEduItem = () => {
-    edu_item = [];
-    for (let i = 1; i <= 49; i++) {
-      if (i >= 45) {
-        edu_item.push({
-          id: i,
-          Elements: <div></div>,
-        });
-      } else {
-        edu_item.push({
-          id: i,
-          Elements: (
-            <img
-              className="img-sizing"
-              src={`/refs/edu_${i}.png`}
-              alt={`edu_${i}`}
-            />
-          ),
-        });
+  addItems = () => {
+    items = [];
+    if (this.state.selectedContent === 1) {
+      for (let i = 1; i <= 49; i++) {
+        if (i >= 45) {
+          items.push({
+            id: i,
+            Elements: <div></div>,
+          });
+        } else {
+          items.push({
+            id: i,
+            Elements: (
+              <img
+                className="img-sizing"
+                src={`/refs/edu_${i}.png`}
+                alt={`edu_${i}`}
+              />
+            ),
+          });
+        }
       }
+      return this.list_eduItems("Edu");
+    } else if (this.state.selectedContent === 2) {
+      for (let i = 3; i <= 60; i++) {
+        if (i !== 10) {
+          if (i >= 3 && i <= 9) {
+            items.push({
+              Elements: (
+                <img
+                  className="img-sizing"
+                  src={`https://www.ablestor.com/public/image/reference/b/refer_0${i}.png`}
+                  alt={`company_0${i}`}
+                />
+              ),
+            });
+          } else if (i >= 57) {
+            items.push({
+              Elements: <div></div>,
+            });
+          } else {
+            items.push({
+              Elements: (
+                <img
+                  className="img-sizing"
+                  src={`https://www.ablestor.com/public/image/reference/b/refer_${i}.png`}
+                  alt={`company_${i}`}
+                />
+              ),
+            });
+          }
+        }
+      }
+      return this.list_eduItems("Company");
+    } else if (this.state.selectedContent === 3) {
+      for (let i = 1; i <= 35; i++) {
+        if (i >= 30) {
+          items.push({
+            Elements: <div></div>,
+          });
+        } else {
+          items.push({
+            Elements: (
+              <img
+                className="img-sizing"
+                src={`/refs/public_${i}.png`}
+                alt={`public_${i}`}
+              />
+            ),
+          });
+        }
+      }
+      return this.list_eduItems("Public");
+    } else if (this.state.selectedContent === 4) {
+      for (let i = 1; i <= 42; i++) {
+        if (i >= 38) {
+          items.push({
+            Elements: <div></div>,
+          });
+        } else {
+          items.push({
+            Elements: (
+              <img
+                className="img-sizing"
+                src={`/refs/media_${i}.png`}
+                alt={`media_${i}`}
+              />
+            ),
+          });
+        }
+      }
+      return this.list_eduItems("Media");
+    } else if (this.state.selectedContent === 5) {
+      for (let i = 1; i <= 14; i++) {
+        if (i >= 10) {
+          items.push({
+            Elements: <div></div>,
+          });
+        } else {
+          items.push({
+            Elements: (
+              <img
+                className="img-sizing"
+                src={`/refs/social_${i}.png`}
+                alt={`social_${i}`}
+              />
+            ),
+          });
+        }
+      }
+      return this.list_eduItems("Social");
     }
-    return this.list_eduItems();
   };
 
-  list_eduItems() {
-    return (
-      <Container>
-        <Row>
-          {edu_item.map((Item) => {
-            if (Item.id >= 1 && Item.id <= 7) {
-              return <Col key={Item.id}>{Item.Elements}</Col>;
-            }
-            if (Item.id >= 8 && Item.id <= 14) {
-              return <Col key={Item.id}>{Item.Elements}</Col>;
-            }
-            if (Item.id >= 15 && Item.id <= 21) {
-              return <Col key={Item.id}>{Item.Elements}</Col>;
-            }
-            if (Item.id >= 22 && Item.id <= 28) {
-              return <Col key={Item.id}>{Item.Elements}</Col>;
-            }
-            if (Item.id >= 29 && Item.id <= 35) {
-              return <Col key={Item.id}>{Item.Elements}</Col>;
-            }
-            if (Item.id >= 36 && Item.id <= 42) {
-              return <Col key={Item.id}>{Item.Elements}</Col>;
-            }
-            if (Item.id >= 43 && Item.id <= 49) {
-              return <Col key={Item.id}>{Item.Elements}</Col>;
-            }
-          })}
-        </Row>
-      </Container>
-    );
+  list_eduItems(type) {
+    if (type === "Edu") {
+      return (
+        <Container>
+          <Row>
+            {items.map((Item) => {
+              if (Item.id >= 1 && Item.id <= 7) {
+                return <Col key={Item.id}>{Item.Elements}</Col>;
+              }
+              if (Item.id >= 8 && Item.id <= 14) {
+                return <Col key={Item.id}>{Item.Elements}</Col>;
+              }
+              if (Item.id >= 15 && Item.id <= 21) {
+                return <Col key={Item.id}>{Item.Elements}</Col>;
+              }
+              if (Item.id >= 22 && Item.id <= 28) {
+                return <Col key={Item.id}>{Item.Elements}</Col>;
+              }
+              if (Item.id >= 29 && Item.id <= 35) {
+                return <Col key={Item.id}>{Item.Elements}</Col>;
+              }
+              if (Item.id >= 36 && Item.id <= 42) {
+                return <Col key={Item.id}>{Item.Elements}</Col>;
+              }
+              if (Item.id >= 43 && Item.id <= 49) {
+                return <Col key={Item.id}>{Item.Elements}</Col>;
+              }
+            })}
+          </Row>
+        </Container>
+      );
+    }
+    if (type === "Company") {
+      return (
+        <Container>
+          <Row>
+            {items.map((Item, idx) => {
+              if (idx >= 0 && idx <= 6) {
+                return <Col key={idx}>{Item.Elements}</Col>;
+              }
+              if (idx >= 7 && idx <= 13) {
+                return <Col key={idx}>{Item.Elements}</Col>;
+              }
+              if (idx >= 14 && idx <= 20) {
+                return <Col key={idx}>{Item.Elements}</Col>;
+              }
+              if (idx >= 21 && idx <= 27) {
+                return <Col key={idx}>{Item.Elements}</Col>;
+              }
+              if (idx >= 28 && idx <= 35) {
+                return <Col key={idx}>{Item.Elements}</Col>;
+              }
+              if (idx >= 35 && idx <= 41) {
+                return <Col key={idx}>{Item.Elements}</Col>;
+              }
+              if (idx >= 42 && idx <= 48) {
+                return <Col key={idx}>{Item.Elements}</Col>;
+              }
+              if (idx >= 49 && idx <= 55) {
+                return <Col key={idx}>{Item.Elements}</Col>;
+              }
+            })}
+          </Row>
+        </Container>
+      );
+    }
+    if (type === "Public") {
+      return (
+        <Container>
+          <Row>
+            {items.map((Item, idx) => {
+              if (idx >= 0 && idx <= 6) {
+                return <Col key={idx}>{Item.Elements}</Col>;
+              }
+              if (idx >= 7 && idx <= 13) {
+                return <Col key={idx}>{Item.Elements}</Col>;
+              }
+              if (idx >= 14 && idx <= 20) {
+                return <Col key={idx}>{Item.Elements}</Col>;
+              }
+              if (idx >= 21 && idx <= 27) {
+                return <Col key={idx}>{Item.Elements}</Col>;
+              }
+              if (idx >= 28 && idx <= 34) {
+                return <Col key={idx}>{Item.Elements}</Col>;
+              }
+            })}
+          </Row>
+        </Container>
+      );
+    }
+    if (type === "Media") {
+      return (
+        <Container>
+          <Row>
+            {items.map((Item, idx) => {
+              if (idx >= 0 && idx <= 6) {
+                return <Col key={idx}>{Item.Elements}</Col>;
+              }
+              if (idx >= 7 && idx <= 13) {
+                return <Col key={idx}>{Item.Elements}</Col>;
+              }
+              if (idx >= 14 && idx <= 20) {
+                return <Col key={idx}>{Item.Elements}</Col>;
+              }
+              if (idx >= 21 && idx <= 27) {
+                return <Col key={idx}>{Item.Elements}</Col>;
+              }
+              if (idx >= 28 && idx <= 34) {
+                return <Col key={idx}>{Item.Elements}</Col>;
+              }
+              if (idx >= 35 && idx <= 42) {
+                return <Col key={idx}>{Item.Elements}</Col>;
+              }
+            })}
+          </Row>
+        </Container>
+      );
+    }
+    if (type === "Social") {
+      return (
+        <Container>
+          <Row>
+            {items.map((Item, idx) => {
+              if (idx >= 0 && idx <= 6) {
+                return <Col key={idx}>{Item.Elements}</Col>;
+              }
+              if (idx >= 7 && idx <= 13) {
+                return <Col key={idx}>{Item.Elements}</Col>;
+              }
+            })}
+          </Row>
+        </Container>
+      );
+    }
   }
 
   render() {
@@ -117,7 +312,7 @@ export default class Reference extends Component {
                 <ul className="reference_category">{this.listCategory()}</ul>
                 <br />
                 <br />
-                {this.addEduItem()}
+                {this.addItems()}
               </Col>
             </Row>
           </Container>
